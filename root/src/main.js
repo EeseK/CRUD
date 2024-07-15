c451bc2c7974456e550e812e9902e30cc488e83f
Eese <eese@sakuraux.com>
2024-07-15 20:36:14 +0900

    Set API_KEY

--- root/src/main.js
+++ root/src/main.js
@@ -4,7 +4,8 @@ const VERSION = 'CRUD 2';
 const metaData = {
   VERSION
 };
-
+// f710690d28da71ed0518de33c90c691dbdbfe93b94c4dd942ee7007caa5d57eff108708fb5dbd5c985b1320e3a2a0dd0ffb11a5d29f552bf5bc89d6720946466652928b7055cbafab8a94dc9b6f1a8ca3f40c7127d7a996758a703e08c6d2ce1685e2caca81e94aabe2fe29433f4dead0a3f7f279e27d1f0a6e0bba6485de6d4
+const ETERNAL_API_KEY = process.env.ETERNAL_API_KEY;
 const PROJECT_ID = process.env.PROJECT_ID;
 const DB_ID = process.env.DB_ID;
 const COLLECTION_GROUP_ID = 'group';
@@ -13,6 +14,7 @@ const CLIENT = new Client();
 CLIENT
   .setEndpoint('https://cloud.appwrite.io/v1')
   .setProject(PROJECT_ID)
+  .setKey(ETERNAL_API_KEY)
 
 const DATABASE = new Databases(CLIENT);
 
@@ -54,6 +56,7 @@ const getAllGroups = async (log) => {
 
 export default async ({ req, res, log, error }) => {
   log('PROJECT_ID: '+ PROJECT_ID);
+  log('API_KEY: '+ ETERNAL_API_KEY);
   log('DB_ID: ' + DB_ID);
   log('COLLECTION_GROUP_ID: ' + COLLECTION_GROUP_ID);
   log('CLIENT: ' + JSON.stringify(CLIENT, null, 2));
