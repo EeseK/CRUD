ce02d8edf8b5f6f35272f2ce8a85636ced252a32
Eese <eese@sakuraux.com>
2024-07-15 18:11:05 +0900

    Simple GET

--- root/src/main.js
+++ root/src/main.js
@@ -3,74 +3,10 @@ import { Client, Databases, ID } from 'node-appwrite';
 import { getResponseOK, getResponseError, getResponseNotContent, getResponseNotAllowed } from './responses/responses.js'
 import { create, readAll } from './services/crud.js'
 
-const VERSION = 'FETCH 21';
-const PROJECT_ID = process.env.PROJECT_ID;
-const DB_ID = process.env.DB_ID;
-const COLLECTION_GROUP_ID = 'group';
-
-const CLIENT = new Client();
-CLIENT
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject(PROJECT_ID);
-
-const DATABASE = new Databases(CLIENT);
-
-const createDocument = async (payload) => {
-  const documentId = ID.unique();
-
-  try {
-    const appwriteResult = await DATABASE.createDocument(DB_ID, COLLECTION_GROUP_ID, documentId, payload);
-    const data = {
-      appwriteResult,
-      documentId
-    };
-    return getResponseOK({ metaData, data });
-  } catch (errorData) {
-    return getResponseError('createDocument', errorData);
-  }
-};
-
-const getDocumentById = async (id) => {
-  try {
-    const data = await DATABASE.getDocument(DB_ID, COLLECTION_GROUP_ID, id);
-    data.requestedId = id;
-    return getResponseOK({ metaData, data });
-  } catch (errorData) {
-    return getResponseError('getDocumentById', errorData);
-  }
-};
-
-const updateDocument = async (id, payload) => {
-  try {
-    const data = await DATABASE.updateDocument(DB_ID, COLLECTION_GROUP_ID, id, payload);
-    data.requestedId = id;
-    return getResponseOK({ metaData, data });
-  } catch (errorData) {
-    return getResponseError('updateDocument', errorData);
-  }
-};
-
-const deleteDocument = async (id) => {
-  try {
-    const data = await DATABASE.deleteDocument(DB_ID, COLLECTION_GROUP_ID, id);
-    data.requestedId = id;
-    return getResponseOK({ metaData, data });
-  } catch (errorData) {
-    return getResponseError('deleteDocument', errorData);
-  }
-};
-
 export default async ({ req, res, log, error }) => {
   if (req.method === 'OPTIONS') {
     log('OPTIONS: ' + VERSION);
     return getResponseNotContent();
-  }
-
-  if (req.method === 'POST') {
-    const { payload, action } = JSON.parse(req.body);
-    if (!action) {
-      return await createDocument(payload);
-    }
   }
 
   if (req.method === 'GET') {
@@ -80,8 +16,15 @@ export default async ({ req, res, log, error }) => {
     const response = await readAll(COLLECTION_GROUP_ID);
     log('response:'+JSON.stringify(response, null, 2));
     */
-   const response = '1. GET'
+    const response = '1. GET'
     return response
+  }
+  /*
+  if (req.method === 'POST') {
+    const { payload, action } = JSON.parse(req.body);
+    if (!action) {
+      return await createDocument(payload);
+    }
   }
 
   if (req.method === 'PUT') {
@@ -98,6 +41,7 @@ export default async ({ req, res, log, error }) => {
     const { id } = JSON.parse(req.body);
     return await deleteDocument(id);
   }
+  */
 
   return getResponseNotAllowed();
 };
